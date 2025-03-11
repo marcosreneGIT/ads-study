b34f9677b36ccf367fd123c1ff6e670aacaf8f73
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2025-03-11 11:14:56 -0300

    exemplo_005
    Operadores Aritméticos de Incremento e Decremento

---
+++ 2025.1/algoritmo-logica/topico-2/exemplos/c/exemplo_005.c
@@ -0,0 +1,16 @@
+int main()
+{
+    int iValor = 0;
+    iValor++; // Neste momento iValor = 1
+
+    iValor = 0;
+    iValor = iValor + 1; // É o mesmo que ++
+
+    int dValor = 10;
+    dValor--; // Neste momento dValor = 9
+
+    dValor = 10;
+    dValor = dValor - 1; // É o mesmo que --
+
+    return 0;
+}
