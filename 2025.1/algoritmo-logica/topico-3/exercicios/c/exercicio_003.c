c7acbef563c91c4d4591050b9d8973c2f95623c8
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2025-04-07 16:18:44 -0300

    exercicio_003.c
    Escada de if-else-if

---
+++ 2025.1/algoritmo-logica/topico-3/exercicios/c/exercicio_003.c
@@ -0,0 +1,28 @@
+// Escada de if-else-if
+
+#include <stdio.h>
+
+int main(void)
+{
+    int idade;
+
+    printf("Informe sua idade: ");
+    scanf("%d", &idade);
+
+    if (idade >= 0 && idade < 16)
+        printf("Crianca.");
+
+    else if (idade > 15 && idade < 22)
+        printf("Adolescente.");
+
+    else if (idade > 22 && idade < 60)
+        printf("Adulto.");
+
+    else if (idade > 60)
+        printf("Idoso.");
+
+    else
+        printf("Idade invalida.");
+
+    return 0;
+}
