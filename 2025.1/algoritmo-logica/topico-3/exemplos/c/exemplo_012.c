b9dd5ff053e1dcdecdab41a026a518cb40a847e6
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2025-04-14 14:16:21 -0300

    exemplo_012.c
    For

---
+++ 2025.1/algoritmo-logica/topico-3/exemplos/c/exemplo_012.c
@@ -0,0 +1,16 @@
+// For
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    printf("Exemplo de estrutura de repeticao for:\n\n");
+
+    for (int contador = 1; contador <= 10; contador++)
+    {
+        printf("Valor do contador %d\n", contador);
+    }
+
+    return 0;
+}
