37858c927c72beb1885135857ec43f5782a375ac
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2025-06-18 16:37:31 -0300

    exemplo_014.c

---
+++ 2025.1/algoritmo-logica/topico-3/exemplos/c/exemplo_014.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+    printf("\nExemplo de estrutura de repeticao do-while\n");
+
+    int contador = 0; 
+
+    do{
+        contador ++;
+        printf("\nO valor do contador e = %d", contador);
+    }
+    while(contador < 10);
+
+    printf("\n\nO valor do contador na saida do laco = %d", contador);
+
+    return 0;
+
+}
