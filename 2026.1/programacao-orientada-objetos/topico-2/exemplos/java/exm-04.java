4b9371473e855e68204f3f606c4e003290d8122a
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2026-02-19 10:21:26 -0300

    Update exm-04.java

--- 2026.1/programacao-orientada-objetos/topico-2/exemplos/java/exm-04.java
+++ 2026.1/programacao-orientada-objetos/topico-2/exemplos/java/exm-04.java
@@ -3,7 +3,8 @@
 public class Main{
     public static void(String[]args){
         Casa casa01 = new Casa();
-        Casa casa02 = new Casa();
-        Casa casa03 = new Casa();
+        Casa casa02 = new Casa(100.0);
+        Casa casa03 = new Casa("Rua Zero");
+        Casa casa04 = new Casa(150.0, "Rua Um");
     }
 }
