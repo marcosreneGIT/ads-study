ed2f25f2d6e38cd6c7355a0f8164e6e3eb8e928d
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2026-08-17 11:39:48 -0300

    Create exemplo-02.php

---
+++ 2026.2/programação-back-end/topico-2/exemplos/php/exemplo-02.php
@@ -0,0 +1,10 @@
+<html>
+    <head></head>
+    <body>
+        <?php
+            $dia = "25";
+            $mes = "Dezembro";
+            ?>
+            <p> O natal e no dia <?php echo $dia; ?> de <?php echo $mes; ?>. </p>
+    </body>
+</html>
