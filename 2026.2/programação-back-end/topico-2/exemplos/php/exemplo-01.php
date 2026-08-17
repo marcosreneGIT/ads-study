9280ec337e022e7d24cb9f048a31a7c9c6d085e4
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2026-08-17 11:39:46 -0300

    Create exemplo-01.php

---
+++ 2026.2/programação-back-end/topico-2/exemplos/php/exemplo-01.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <body>
+            <?php
+            // o programa esta pronto
+            echo "<center> <h1> Meu primeiro condigo php! </h1> <center>";
+            ?>
+        </body>
+    </head>
+</html>
